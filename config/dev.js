3579bbf982e5fa26adf80c2250b7d9e8df214517
Vinicius Carneiro <vinny@predictivehire.com>
2016-12-19 12:01:49 +1100

    new config

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
     app: {
-        // apiURL: 'http://127.0.0.1:3000/',
-        apiURL: 'http://192.168.1.104:3000/',
+        apiURL: 'http://127.0.0.1:3000/',
+        // apiURL: 'http://192.168.1.104:3000/',
         // apiURL: '/',
     },
     api: {
